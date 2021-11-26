0ec93f815b075b93e303881818020055fd775eeb
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-11-26 21:57:38 +0530

    Adding Hover Animation to icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -68,6 +68,28 @@ const NavMenu = styled.div`
         span {
             font-size: 13px;
             letter-spacing: 1.42px;
+            position: relative;
+
+            &:after {
+                content: "";
+                height: 2px;
+                background: white;
+                position: absolute;
+                left: 0;
+                right: 0;
+                bottom: -6px;
+                opacity: 0;
+                transform-origin: left center;
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                transform: scaleX(0);
+            }
+        }
+
+        &:hover {
+            span:after {
+                transform: scaleX(1);
+                opacity: 1;
+            }
         }
     }
 `
