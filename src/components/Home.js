4cabb9b8549a16920573cff632d938031df088a2
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-11-28 08:37:47 +0530

    Adding Viewers Section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import ImgSlider from './ImgSlider';
+import Viewers from './Viewers';
 
 function Home() {
     return (
         <Container>
             <ImgSlider />
+            <Viewers />
         </Container>
     )
 }
