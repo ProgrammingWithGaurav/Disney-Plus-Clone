5b6b9df43c086a72cc7deb24ef511031d0fa6be7
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-12-01 21:16:16 +0530

    adding favicon and title

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import ImgSlider from './ImgSlider';
 import Viewers from './Viewers';
 import Movies from './Movies';
+import db from '../firebase';
 
 function Home() {
+    useEffect(() => {
+        db.collection('movies').onSnapshot((snapshot) => {
+            console.log(snapshot);
+        })
+    }, [])
+    
     return (
         <Container>
             <ImgSlider />
             <Viewers />
-            <Movies />  
+            <Movies />
         </Container>
     )
 }
